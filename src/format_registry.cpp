a8f779d13a221c148c068ed378ad4e563a5c7146
Nelonn <42481486+Nelonn@users.noreply.github.com>
2026-09-08 00:38:00 +0400

    fix disabled tiff

--- src/format_registry.cpp
+++ src/format_registry.cpp
@@ -97,7 +97,9 @@ void registerBuiltInFormats(FormatRegistry* registry) noexcept {
   registry->registerFormat(&FORMAT_GIF);
   registry->registerFormat(&FORMAT_TGA);
   registry->registerFormat(&FORMAT_BMP);
+#if defined(OPENMEDIA_TIFF)
   registry->registerFormat(&FORMAT_TIFF);
+#endif
   registry->registerFormat(&FORMAT_ICNS);
 #if defined(OPENMEDIA_DDS)
   registry->registerFormat(&FORMAT_DDS);
